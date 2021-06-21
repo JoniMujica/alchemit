6aafe13936c7f6c39df13c18bd14b2a468127e29
Jonathan Mujica <jonimujica3@hotmail.com>
2021-06-21 16:10:15 -0300

    Listas & operaciones

---
+++ C5 arreglos y matrices/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace C5_arreglos_y_matrices
+{
+    class Program
+    {
+        static void mostrarVec(int[] v)
+        {
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.Write(v[i] + " ");
+            }
+        }
+
+        static void mostrarMatriz(int[,] m)
+        {
+            for (int i = 0; i < m.GetLength(0); i++) //GetLength(n) me trae la logitud del array en su dimension
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    Console.Write(m[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+        }
+        static void Main(string[] args)
+        {
+            int[] vector = { 1, 2, 3, 4, 5, 6 };
+            int[,] matriz = new int[4, 5]; //para declarar una matriz, si o si debo pones una coma "[,]" y luego en el NEW determinar las filas y columnas
+            mostrarVec(vector);
+
+            Console.WriteLine("El elemento maximo es {0}, y el elemento minimo es {1}", vector.Max(),vector.Min());  //con min y max , obtengo los valores minimos y maximos de los elementos del array
+
+            Console.WriteLine("El promedio del vector es: {0}", vector.Average()); //con Average obtengo el promedio del vector (la suma de los valores de los elementos / la cantidad de elementos del array
+
+
+            ///////////////////////////////////CARGO LA MATRIZ DE DATOS/////////////////////////////////
+            for (int i = 0; i < matriz.GetLength(0); i++) //GetLength(n) me trae la logitud del array en su dimension
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = (i + j) % 10;
+                }
+            }
+            mostrarMatriz(matriz);
+
+
+            ///MOVER EL PRIMER ELEMENTO DEL VECTOR, AL ULTIMO///
+            int prov = vector[0];
+            for (int i = 0; i < vector.Length-1; i++)
+            {
+                vector[i] = vector[i + 1];
+            }
+            vector[vector.Length - 1] = prov;
+
+            Console.WriteLine("Res: ");
+            mostrarVec(vector);
+        }
+    }
+}
---
+++ C6 Listas/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace C6_Listas
+{
+    class Program
+    {
+        static void mostrarLista(List<int> lista)
+        {
+            foreach (int x in lista)
+            {
+                Console.Write(x + " ");
+            }
+            Console.WriteLine("");
+        }
+        static void Main(string[] args)
+        {
+            List<int> Lista1 = new List<int>();
+            List<int> Lista2 = new List<int>();
+            Lista1.Add(5);
+            Lista1.Add(7);
+            Lista1.Add(1);
+            Lista1.Add(3);
+            Lista1.Add(9);
+            Lista1.Add(5);
+
+            Lista2.Add(8);
+            Lista2.Add(6);
+            Lista2.Add(2);
+            Lista2.Add(4);
+            Lista2.Add(0);
+
+            Console.WriteLine("Lista 1:");
+            mostrarLista(Lista1);
+            Console.WriteLine("Lista 2:");
+            mostrarLista(Lista2);
+
+            //Agrego un elemento a la lista en una posicion X
+            Lista2.Insert(4, 10); //agrego en la posicion 4 el numero 10
+            mostrarLista(Lista2);
+
+            //Mostrar/Recorrer lista
+
+            for (int i = 0; i < Lista2.Count; i++)
+            {
+                Console.Write(Lista2[i] + " "); 
+            }
+
+
+            //Concatenar listas
+            List<int> Lista3 = new List<int>();
+            Lista3.AddRange(Lista2);
+            Lista3.AddRange(Lista1);
+            Console.WriteLine("Lista 3:");
+            mostrarLista(Lista3);
+
+            //Buscar elemento de la lista
+
+            Console.WriteLine("El 10 esta en la posicion {0}", Lista3.IndexOf(10));
+            // Ordenar lista
+            Lista3.Sort(); //ordena la lista de menor a mayor
+            Console.WriteLine("Lista 3 ordenada");
+            mostrarLista(Lista3);
+            Console.WriteLine(Lista3.BinarySearch(10)); //para usar el "BinarySearch" la lista o vector, deben estar ordenados
+        }
+    }
+}
