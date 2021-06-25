704f1cba45e7b291b7d5eddb23a7a1649138ce5c
Jonathan Mujica <jonimujica3@hotmail.com>
2021-06-25 20:23:33 -0300

    listas, uso de predicados

---
+++ listas - uso de predicados/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace listas___uso_de_predicados
+{
+    class Program
+    {
+
+        static void mostrarLista(List<int> lista)
+        {
+            foreach (int x in lista)
+            {
+                Console.Write(x + " ");
+            }
+            Console.WriteLine("");
+        }
+        static void Main(string[] args)
+        {
+            List<int> lista1 = new List<int>();
+            for (int i = 0; i <= 20; i++)
+            {
+                lista1.Add(i);
+            }
+            Console.Write("Lista 1: ");
+            mostrarLista(lista1);
+        }
+
+
+    }
+}
