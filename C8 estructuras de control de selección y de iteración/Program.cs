c29504f514a8574d397ad6230df9d7ae7f9b77e2
Jonathan Mujica <jonimujica3@hotmail.com>
2021-07-11 01:14:20 -0300

    estructura de saltos

---
+++ C8 estructuras de control de selección y de iteración/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C8_estructuras_de_control_de_selección_y_de_iteración
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>();
+            lista.Add(30);
+            lista.Add(27);
+            lista.Add(20);
+            lista.Add(17);
+            lista.Add(5);
+            foreach (int x in lista)
+            {
+                if (x%3 == 0)
+                {
+                    if (x % 2 == 0)
+                    {
+                        Console.WriteLine("el numero {0} es divisible por 6", x);
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero {0} es divisible por 3",x);
+                    }
+                }
+                else
+                {
+                    if (x%2 == 0)
+                    {
+                        Console.WriteLine("el numero {0} es divisible por 2", x);
+                    }
+                    else
+                    {
+                        Console.WriteLine("el numero {0} no es divisible por 3 ni por 2", x);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C9 ESTRUCTURA DE SALTO/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C9_ESTRUCTURA_DE_SALTO
+{
+    class Program
+    {
+        static void mostrarLista(List<int> v)
+        {
+            foreach (int x in v)
+            {
+                if (x % 2 == 0)
+                {
+                    //continue;
+                    Console.Write("{0} ", x);
+                }
+                //Console.Write("{0} ", x);
+            }
+            Console.WriteLine("");
+        }
+
+        //metodo 1
+        /*
+        static void buscarNum(int x,List<int> v)
+        {
+            for (int i = 0; i < v.Count; i++)
+            {
+                if (v[i] == x)
+                {
+                    Console.WriteLine("El numero {0} esta en la lista",x);
+                    break;
+                }
+                Console.WriteLine("{0} ", i);
+            }
+            Console.WriteLine("");
+        }*/
+        static void buscarNum(int x, List<int> v)
+        {
+            bool enc = false;
+            for (int i = 0; i < v.Count && !enc; i++)
+            {
+                if (v[i] == x)
+                {
+                    Console.WriteLine("El numero {0} esta en la lista", x);
+                    enc = true;
+                }
+                Console.WriteLine("{0} ", i);
+            }
+            Console.WriteLine("");
+        }
+        static void Main(string[] args)
+        {
+            List<int> Lista = new List<int>();
+            for (int i = 0; i <= 40; i++)
+            {
+                Lista.Add(i);
+            }
+            mostrarLista(Lista);
+            buscarNum(5, Lista);
+
+            // aca un error que hay que cuidarse de usar continue (siempre sera verdadera y entra en un loop infinito)
+            int inx = 0;
+            while (inx < 40)
+            {
+                if (Lista[inx] != 6)
+                    continue;
+                Console.WriteLine("El numero 6 esta en la lista");
+
+                inx++;
+            }
+        }
+    }
+}
